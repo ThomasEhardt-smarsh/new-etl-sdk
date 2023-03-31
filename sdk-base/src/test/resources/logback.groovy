e18c5c2664a8251c4b48ab6500eab1889dc31d47
Thomas Ehardt <thomas.ehardt@smarsh.com>
2023-03-31 15:39:40 -0500

    removed sdk-base in favor of vams-etl-sdk

--- sdk-base/src/test/resources/logback.groovy
+++
@@ -1,9 +0,0 @@
-appender('CONSOLE', ConsoleAppender) {
-    encoder(PatternLayoutEncoder) {
-        pattern = '%d %highlight(%-5level) [%thread] %logger{46} - %msg%n'
-    }
-}
-
-logger('com.smarsh', TRACE)
-
-root(TRACE, ['CONSOLE'])
